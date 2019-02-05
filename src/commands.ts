d9851f94c2b9f93273054ca98483bbb9113d9624
s.siefke.ext@bitworkers.eu <simon.siefke@gmail.com>
2019-02-05 23:17:13 +0100

    feat: start & stop commands actually working

--- src/commands.ts
+++ src/commands.ts
@@ -1,23 +1,45 @@
 import * as vscode from 'vscode'
 import * as cp from 'child_process'
-
+import * as fs from 'fs'
+import * as path from 'path'
 /**
  * runs a command
  */
 async function runCommand(command: string) {
-  console.log(`[ddev] ${JSON.stringify(vscode.workspace.workspaceFolders)}`)
-
-  const hasDdevFolderOpen = vscode.workspace.rootPath
-  if (!hasDdevFolderOpen) {
-    vscode.window.showErrorMessage(`[ddev] you need to open a project first`, { modal: true })
-    return
-  }
-  cp.exec(command, (error, stdout) => {
-    if (error) {
-      vscode.window.showErrorMessage(`[ddev] ${stdout}`)
-    } else {
-      vscode.window.showInformationMessage(`[ddev] ${stdout}`)
+  return new Promise((resolve, reject) => {
+    const hasDdevFolderOpen = vscode.workspace.rootPath
+    if (!hasDdevFolderOpen) {
+      vscode.window.showErrorMessage(`[ddev] you need to open a project first`, { modal: true })
+      reject()
     }
+    const hasMultipleFoldersOpen = vscode.workspace.workspaceFolders.length >= 2
+    if (hasMultipleFoldersOpen) {
+      vscode.window.showErrorMessage(
+        `[ddev] this extension currently only works with one open folder`,
+        { modal: true }
+      )
+      reject()
+    }
+
+    const folderPath = vscode.workspace.workspaceFolders[0].uri.fsPath
+    const hasDdevFolderInside = fs.existsSync(path.join(folderPath, '.ddev'))
+    if (!hasDdevFolderInside) {
+      vscode.window.showInformationMessage(
+        `[ddev] Couldn't find a .ddev folder in your workspace`,
+        {
+          modal: true,
+        }
+      )
+      reject()
+    }
+    cp.exec(`cd ${folderPath} && ${command}`, (error, stdout) => {
+      if (error) {
+        vscode.window.showErrorMessage(`[ddev] ${stdout}`)
+        reject()
+      } else {
+        resolve()
+      }
+    })
   })
 }
 
@@ -27,11 +49,37 @@ type Command = () => void
  * these are the commands available from the command palette
  */
 const commands: { [key: string]: Command } = {
-  ddevStart() {
-    runCommand('ddev start')
+  async ddevStart() {
+    await vscode.window.withProgress(
+      {
+        location: vscode.ProgressLocation.Notification,
+        title: 'Starting ddev container...',
+        cancellable: true,
+      },
+      (progress, token) => {
+        token.onCancellationRequested(() => {
+          console.log('User canceled the long running operation')
+        })
+        return runCommand('ddev start')
+      }
+    )
+    vscode.window.showInformationMessage(`ddev container started`)
   },
-  ddevStop() {
-    runCommand('ddev stop')
+  async ddevStop() {
+    await vscode.window.withProgress(
+      {
+        location: vscode.ProgressLocation.Notification,
+        title: 'Stopping ddev container...',
+        cancellable: true,
+      },
+      (progress, token) => {
+        token.onCancellationRequested(() => {
+          console.log('User canceled the long running operation')
+        })
+        return runCommand('ddev stop')
+      }
+    )
+    vscode.window.showInformationMessage(`ddev container stopped`)
   },
   ddevComposerInstall() {
     runCommand('ddev composer install')
